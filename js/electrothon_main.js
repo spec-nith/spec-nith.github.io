a34ed6756c35c59c6de40458908ec71b65a169cc
S.P.E.C <57700332+spec-nith@users.noreply.github.com>
2020-11-05 12:52:42 +0530

    Update electrothon_main.js

--- js/electrothon_main.js
+++ js/electrothon_main.js
@@ -1,10 +1,10 @@
 document.addEventListener('DOMContentLoaded', () => {
 
   // Unix timestamp (in seconds) to count down to
-  var twoDaysFromNow = (new Date().getTime() / 1000) + (86400 * 2) +1;
+  var DaysFromNow = (new Date().getTime() / 1000) + (86400 * 93) +1;
 
   // Set up FlipDown
-  var flipdown = new FlipDown(twoDaysFromNow)
+  var flipdown = new FlipDown(DaysFromNow)
 
     // Start the countdown
     .start()
